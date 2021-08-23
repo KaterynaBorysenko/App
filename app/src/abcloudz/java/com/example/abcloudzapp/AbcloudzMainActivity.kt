b437e500ed7dcd317352ac74fb87b2dc8741094b
Kateryna Borysenko <51036990+KaterynaBorysenko@users.noreply.github.com>
2021-08-23 03:25:13 +0300

    Initial commit

---
+++ app/src/abcloudz/java/com/example/abcloudzapp/AbcloudzMainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.abcloudzapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AbcloudzMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_abcloudz_main)
+        setTitle(R.string.app_title)
+    }
+}
